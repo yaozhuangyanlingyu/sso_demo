e0dbdeddf9f4d491e5faa392ddc2a9eb528c96b6
root <root@localhost.localdomain>
2022-07-10 22:11:01 +0800

    test

--- php_cas_demo/config.php
+++ php_cas_demo/config.php
@@ -19,15 +19,16 @@ function p($arr){
 
 # 1 CAS Server 主机域名
 # 此配置是你搭建的CAS SSO SERVER服务的域名
-$cas_host = '192.168.1.112';
+//$cas_host = '192.168.1.112';
+$cas_host = 'localhost';
 
 # 2 CAS Server 路径
 # 此配置是你搭建的CAS SSO SERVER服务的路径
-$cas_context = '/cas/casbin/aplum_mis/';
+$cas_context = '/cas/casbin/admin_manage/';
 
 // 3 CAS server 端口
 # 此配置是你搭建的CAS SSO SERVER服务的端口
-$cas_port = 8000;
+$cas_port = 8200;
 
 // 4 CAS server 证书
 # 此配置是你搭建的CAS SSO SERVER服务的证书文件
--- php_cas_demo/index.php
+++ php_cas_demo/index.php
@@ -2,37 +2,38 @@
 require('vendor/autoload.php');
 use \phpCAS;
 
-// Load the settings from the central config file
+// 引入配置文件
 require_once 'config.php';
 
-// Enable debugging
+// 开启phpCAS debug
 phpCAS::setLogger();
 
-// Enable verbose error messages. Disable in production!
+// 启用详细错误消息。在生产中禁用
 phpCAS::setVerbose(true);
 
-// Initialize phpCAS
+# 初始化phpCAS,参数说明：CAS协议版本号、cas server的域名、cas server的端口号、cas server的项目访问路径
 phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
 
-// For production use set the CA certificate that is the issuer of the cert
-// on the CAS server and uncomment the line below
+# 开启设置证书验证。如果是开发环境可将此注释，如果是生产环境为了安全性建议将此开启
 // phpCAS::setCasServerCACert($cas_server_ca_cert_path);
 
-// For quick testing you can disable SSL validation of the CAS server.
-// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
-// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-
+# 不为CAS服务器设置SSL验证
+# 为了快速测试，您可以禁用CAS服务器的SSL验证。此建议不建议用于生产环境。验证CAS服务器对CAS协议的安全性至关重要！
 phpCAS::setNoCasServerValidation();
 
-// force CAS authentication
+// 进行CAS服务验证，这个方法确保用户是否验证过，如果没有验证则跳转到验证界面
+// 这个是强制认证模式，查看 CAS.php 可以找到几种不同的方式：
+# a) forceAuthentication - phpCAS::forceAuthentication();
+# b) checkAuthentication - phpCAS::checkAuthentication();
+# c) renewAuthentication - phpCAS::renewAuthentication();
 phpCAS::forceAuthentication();
 
 // at this step, the user has been authenticated by the CAS server
 // and the user's login name can be read with phpCAS::getUser().
 
-// logout if desired
+// 注销接口(注销后跳转地址)
 if (isset($_REQUEST['logout'])) {
-	phpCAS::logout();
+	phpCAS::logout(['service'=>$_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']]);
 }
 
 // for this test, simply print that the authentication was successfull
